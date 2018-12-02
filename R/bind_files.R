3021de9530ee2b332fe10c268a8464e1047fa560
Colin Quirk <cquirk@uchicago.edu>
2018-12-02 13:18:49 -0600

    bind_files now writes data by file to avoid memory errors

--- R/bind_files.R
+++ R/bind_files.R
@@ -1,17 +1,25 @@
-bind_files = function(fp, ext="csv", find_id=F, pattern=NULL, .id=NULL) {
+bind_files = function(fp, ext="csv", output='', find_id=F, pattern=NULL, .id=NULL) {
   require(readr)
   require(dplyr)
   require(stringr)
+
+  get_file = function(f, append=T) {
+    if (find_id) {
+      temp = read_csv(f) %>%
+        mutate(!!.id := id)
+    } else {
+      temp = read_csv(f)
+    }
+    write_csv(temp, output, append=append)
+  }
 
   files = list.files(path = fp, pattern = paste0('*.', ext), full.names=T)
 
   if (find_id) {
     id = stringr::str_match(files, pattern)[,2]
-    dat = setNames(lapply(files, FUN=read_csv), id) %>%
-      bind_rows(.id = .id)
-  } else {
-    dat = lapply(files, FUN=read_csv) %>% bind_rows()
   }
 
-  dat
+  get_file(files[[1]])
+
+  NULL
 }
