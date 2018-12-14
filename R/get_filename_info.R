9085c3dd5035d326cf3b2da9080ca3e1d9295a64
Colin Quirk <cquirk@uchicago.edu>
2018-12-14 13:32:52 -0600

    add simplify

--- R/get_filename_info.R
+++ R/get_filename_info.R
@@ -1,9 +1,12 @@
-get_filename_info = function(filename, pattern) {
+get_filename_info = function(filename, pattern, simplify=T) {
   require(stringr)
 
   result = stringr::str_match(filename, pattern)[,-1]
-  if (is.null(nrow(result))) {
-    result = as.matrix(result)
+  if (!simplify) {
+    if (is.null(nrow(result))) {
+      result = as.matrix(result)
+    }
+    result = lapply(seq_len(nrow(result)), function(i) result[i,])
   }
-  lapply(seq_len(nrow(result)), function(i) result[i,])
+  result
 }
