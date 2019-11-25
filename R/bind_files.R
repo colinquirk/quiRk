7b2f1eb53402e87e1386d5f01ffdc9a019b2d68e
Colin Quirk <cquirk@uchicago.edu>
2019-11-25 16:54:34 -0600

    take optional additional arguements for read_csv

--- R/bind_files.R
+++ R/bind_files.R
@@ -1,20 +1,20 @@
-bind_files = function(fp, ext="csv", find_id=F, pattern=NULL, .id=NULL) {
+bind_files = function(fp, ext="csv", find_id=F, pattern=NULL, .id=NULL, ...) {
   require(quiRk)
   require(readr)
 
   if (find_id) {
     files = list.files(path = fp, pattern = paste0('*.', ext), full.names=T)
-    dfs = lapply(files, read_csv)
+    dfs = lapply(files, read_csv, ...)
 
     vals = quiRk::get_filename_info(files, pattern, simplify=F)
     dfs = quiRk::add_filename_info(dfs, .id, vals)
   } else if (!is.null(pattern)) {
     # If we aren't finding a subject number, just use pattern to select files
     files = list.files(path = fp, pattern = pattern, full.names=T)
-    dfs = lapply(files, read_csv)
+    dfs = lapply(files, read_csv, ...)
   } else {
     files = list.files(path = fp, pattern = paste0('*.', ext), full.names=T)
-    dfs = lapply(files, read_csv)
+    dfs = lapply(files, read_csv, ...)
   }
 
   dfs %>%
