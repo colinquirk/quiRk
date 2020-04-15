97d08eb8b330f46ca48798f9fbdd70472e53a4a1
Colin Quirk <cquirk@uchicago.edu>
2020-04-14 19:49:31 -0500

    generalize read function

--- R/bind_files.R
+++ R/bind_files.R
@@ -1,20 +1,24 @@
-bind_files = function(fp, ext="csv", find_id=F, pattern=NULL, .id=NULL, ...) {
+bind_files = function(fp, ext="csv", find_id=F, pattern=NULL, .id=NULL, func=NULL, ...) {
   require(quiRk)
   require(readr)
 
+  if (is.null(func)) {
+    func = read_csv
+  }
+
   if (find_id) {
     files = list.files(path = fp, pattern = paste0('*.', ext), full.names=T)
-    dfs = lapply(files, read_csv, ...)
+    dfs = lapply(files, func, ...)
 
     vals = quiRk::get_filename_info(files, pattern, simplify=F)
     dfs = quiRk::add_filename_info(dfs, .id, vals)
   } else if (!is.null(pattern)) {
     # If we aren't finding a subject number, just use pattern to select files
     files = list.files(path = fp, pattern = pattern, full.names=T)
-    dfs = lapply(files, read_csv, ...)
+    dfs = lapply(files, func, ...)
   } else {
     files = list.files(path = fp, pattern = paste0('*.', ext), full.names=T)
-    dfs = lapply(files, read_csv, ...)
+    dfs = lapply(files, func, ...)
   }
 
   dfs %>%
